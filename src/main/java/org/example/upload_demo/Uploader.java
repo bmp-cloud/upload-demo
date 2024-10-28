cffa4f03288714f24baa40bd857184a54ea17d2f
Je-Hwan Yoo <yoojehwan1875@gmail.com>
2024-10-28 11:45:10 +0900

    Update index page and rename HelloServlet to Uploader
    Changed the index page heading to 'JSP Demo' and updated the link to 'File Uploader'. Renamed the HelloServlet to Uploader and adjusted the servlet mapping accordingly. Added an IntelliJ IDEA user interface designer configuration file.

--- src/main/java/org/example/upload_demo/Uploader.java
+++ src/main/java/org/example/upload_demo/Uploader.java
@@ -5,12 +5,12 @@ import java.io.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
-@WebServlet(name = "helloServlet", value = "/hello-servlet")
-public class HelloServlet extends HttpServlet {
+@WebServlet(name = "uploaderServlet", value = "/uploader")
+public class Uploader extends HttpServlet {
     private String message;
 
     public void init() {
-        message = "Hello World!";
+        message = "Hello World~!";
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
